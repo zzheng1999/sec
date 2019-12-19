3e735e1f9b2b576a94643584a02e2f9176a617c0
zhong zheng <zz695920754@gmail.com>
2019-12-19 16:35:01 -0600

    add login page

--- app_hashing.js
+++ app_hashing.js
@@ -4,9 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
+const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -16,18 +17,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-app.use(session({
-  secret: "Our little secret",
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema(
   {
@@ -35,19 +25,11 @@ const userSchema = new mongoose.Schema(
     password: String
   });
 
-userSchema.plugin(passportLocalMongoose);
 
 // use encryption
 //userSchema.plugin(encrypt, {secret: process.env.SECRET});
 
 const User = new mongoose.model("User", userSchema);
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-
-
 
 
 /**************************************** Home ***********************************************/
@@ -70,33 +52,24 @@ app.route("/register")
 )
 
 .post(function(req, res) {
-
-  User.register({username: req.body.username, aactive: false}, req.body.password, function(err, user) {
-    if (err) {
-      console.log(err);
-      res.redirect('/register');
-    }
-    else {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/game");
-      });
-    }
+  const password = req.body.password;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      }
+      else{
+        console.log(err);
+      }
+    });
   });
 });
 
-/**************************************** render index page ***********************************************/
 
-
-app.route("/game")
-
-.get(function(req, res) {
-  if (req.isAuthenticated()) {
-    res.render("index");
-  }
-  else{
-    res.redirect('/login');
-  }
-});
 
 
 
@@ -110,18 +83,20 @@ app.route("/login")
 )
 
 .post(function(req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  req.login(user,function(err) {
-    if(err){
+  User.findOne({email: req.body.username}, function(err, foundUser) {
+    if (err) {
       console.log(err);
     }
-    else{
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/game");
+    else {
+      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          if (result === true) {
+            res.render("secrets");
+          }
+        }
       });
     }
   });
@@ -134,8 +109,7 @@ app.route("/login")
 app.route("/logout")
 
 .get(function(req, res) {
-  req.logout();
-  res.redirect('/');
+  res.render("home");
 });
 
 
